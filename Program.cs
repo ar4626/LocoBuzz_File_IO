1fc2d29d30e1fb71ac50435c5e281d8d353c21dc
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-07 10:57:57 +0530

    [Ankit] Add : Adding a feature to Override the complete text file

--- Program.cs
+++ Program.cs
@@ -29,8 +29,30 @@ namespace File_IO
                 }
 
             }
-            
-        
+
+            //opening the file for writing.  //this will be overriding the complete content
+            using (FileStream fa = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                string dataToWrite = "I study In SRMIST";
+                byte[] dataBytes = Encoding.UTF8.GetBytes(dataToWrite);
+
+                //this writing the string form the begining 
+                fa.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            //Opening the file for Reading.
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = 0;
+
+                //Reading multiple lines in the file 
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                }
+            }
+
         }
     }
 }
