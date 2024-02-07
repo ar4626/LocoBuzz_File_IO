98c4a72dfef5208a12b806050977cb708336c0c0
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-07 08:17:45 +0530

    [Ankit] Initialization : Initialising Project for implementing various operation in FileIO

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File IO Operations.");
+
+            
+
+
+
+        }
+    }
+}
