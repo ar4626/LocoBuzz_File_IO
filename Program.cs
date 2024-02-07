702bc81e735d859d38c1b56ccdd083b4826cd849
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-07 10:55:50 +0530

    [Ankit] Add : Adding the feature to read a text file.

--- Program.cs
+++ Program.cs
@@ -13,10 +13,24 @@ namespace File_IO
         {
             Console.WriteLine("File IO Operations.");
 
+            string filePath = @"F:\LOCOBUZZ\File_IO\sample.txt";
+
+            //Opening a file for Reading 
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = 0;
+
+                //Reading multiple lines in the file 
+
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                }
+
+            }
             
-
-
-
+        
         }
     }
 }
